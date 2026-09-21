8ef66c743484491e45d78d6fd8530fd8ce8efd01
seol ina <78303244+ina-seol@users.noreply.github.com>
2026-09-21 13:52:43 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
-import { defineConfig } from "vite";
+import {
+  defineConfig
+} from "vite";
+
 
 export default defineConfig({
-  base: "/wisdom-maze-prototype/"
+
+  base:
+    "/wisdom-maze-prototype/"
+
 });
