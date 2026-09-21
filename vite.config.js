c54439e78dc5b6931721a248c48228f3ce0c6e38
seol ina <78303244+ina-seol@users.noreply.github.com>
2026-09-21 14:14:46 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,45 @@
-import { defineConfig } from "vite";
-import { fileURLToPath, URL } from "node:url";
+import {
+  defineConfig
+} from "vite";
+
+import {
+  fileURLToPath,
+  URL
+} from "node:url";
+
 
 export default defineConfig({
-  base: "/wisdom-maze-prototype/",
+
+  base:
+    "/wisdom-maze-prototype/",
+
 
   build: {
-    rollupOptions: {
-      input: {
-        main: fileURLToPath(
-          new URL("./index.html", import.meta.url)
-        ),
 
-        admin: fileURLToPath(
-          new URL("./admin.html", import.meta.url)
-        )
+    rollupOptions: {
+
+      input: {
+
+        main:
+          fileURLToPath(
+            new URL(
+              "./index.html",
+              import.meta.url
+            )
+          ),
+
+        admin:
+          fileURLToPath(
+            new URL(
+              "./admin.html",
+              import.meta.url
+            )
+          )
+
       }
+
     }
+
   }
+
 });
